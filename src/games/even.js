3d6b7943ac3abf2e5ea9acaf62c67e0612868145
Your Name~rm <you@example.com>
2024-02-12 22:45:28 +0300

    Step 5 of the Game: "Checking even numbers". removed errors.

--- src/games/even.js
+++ src/games/even.js
@@ -1,21 +1,21 @@
-import generateRandomInteger from "../generateRandomInteger.js";
-import logicGame, { numberOfRounds } from "../logicGame.js";
+import generateRandomInteger from '../generateRandomInteger.js';
+import logicGame, { numberOfRounds } from '../logicGame.js';
 
 const taskForGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (number) => number % 2 === 0;
 
 const newRound = () => {
-    const questionNumber = generateRandomInteger();
-    const correctAnswer = isEven(questionNumber) ? 'yes' : 'no';
-    return [questionNumber, correctAnswer];
+  const questionNumber = generateRandomInteger();
+  const correctAnswer = isEven(questionNumber) ? 'yes' : 'no';
+  return [questionNumber, correctAnswer];
 };
 
 const evenGame = () => {
-    const round = [];
-    for (let i = 0; i < numberOfRounds; i += 1) {
-        round [i] = newRound();
-    }
-    return logicGame(round, taskForGame);
+  const round = [];
+  for (let i = 0; i < numberOfRounds; i += 1) {
+    round[i] = newRound();
+  }
+  return logicGame(round, taskForGame);
 };
 
 export default evenGame;
--- src/logicGame.js
+++ src/logicGame.js
@@ -3,23 +3,23 @@ import readlineSync from 'readline-sync';
 export const numberOfRounds = 3;
 
 const logicGame = (round, taskForGame) => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${name}`);
-    console.log(taskForGame);
-    for (let i = 0; i < numberOfRounds; i += 1) {
-        const [questionNumber, correctAnswer] = round[i];
-        console.log(`Question: ${questionNumber}`);
-        const userAnswer = readlineSync.question ('Your answer: ');
-        if (userAnswer === correctAnswer) {
-            console.log('Correct!');
-        } else {
-            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
-            return;
-        }
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${name}`);
+  console.log(taskForGame);
+  for (let i = 0; i < numberOfRounds; i += 1) {
+    const [questionNumber, correctAnswer] = round[i];
+    console.log(`Question: ${questionNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+      return;
     }
+  }
 
-    console.log(`Congratulations, ${name}`);
+  console.log(`Congratulations, ${name}`);
 };
 
 export default logicGame;
